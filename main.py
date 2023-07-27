475307470c064ef4674f99994e9a34de17f419b4
ryanhlam1219 <63172653+ryanhlam1219@users.noreply.github.com>
2023-07-27 15:46:02 -0700

    Updating main.py to seperate real and test interactions

--- main.py
+++ main.py
@@ -24,33 +24,31 @@ secret: str = config('BINANCE_API_SECRET')
 
 if trading_mode == 'real':
     print("*** Caution: Real trading mode activated ***")
-else:
-    print("Test mode")
-    testScript.executeScript()
-
-if len(sys.argv) > 1:
-    currencies = sys.argv[1].split('_')
+    # Load exchange
+    print("Connecting to {} exchange...".format(exchange_name[0].upper() + exchange_name[1:]))
+    if len(sys.argv) > 1:
+        currencies = sys.argv[1].split('_')
     if len(currencies) > 1:
         currency = currencies[0]
         asset = currencies[1]
+    client = Client.Binance(key,secret)
+    if(client.getConnectivityStatus()):
+        print("Connected.")
+    else:
+        print("Could not establish connection to {} exchange...".format(exchange_name[0].upper() + exchange_name[1:]))
 
-# Load exchange
-print("Connecting to {} exchange...".format(exchange_name[0].upper() + exchange_name[1:]))
-client = Client.Binance(key,secret)
-if(client.getConnectivityStatus()):
-    print("Connected.")
+    client.getAccountStatus()
+    client.getCandleStickData('BTCUSD')
+
+    def signal_handler(signal, frame):
+            print('\nstopping client...')
+            sys.exit(0)
+
+    # Listen for keyboard interrupt event
+    signal.signal(signal.SIGINT, signal_handler)
+    forever = threading.Event()
+    forever.wait()
+    sys.exit(0)
 else:
-    print("Could not establish connection to {} exchange...".format(exchange_name[0].upper() + exchange_name[1:]))
-
-client.getAccountStatus()
-client.getCandleStickData('BTCUSD')
-
-def signal_handler(signal, frame):
-        print('\nstopping client...')
-        sys.exit(0)
-
-# Listen for keyboard interrupt event
-signal.signal(signal.SIGINT, signal_handler)
-forever = threading.Event()
-forever.wait()
-sys.exit(0)
+    print("Test mode")
+    testScript.executeScript()
