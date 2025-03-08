ca07e9daa3bc76375e23d826ddb23f58de8be053
ryanhlam1219 <63172653+ryanhlam1219@users.noreply.github.com>
2025-03-08 13:27:47 -0800

    Modifying Exchange Class to enforce arguments in create_new_order function

--- Exchanges/exchange.py
+++ Exchanges/exchange.py
@@ -1,7 +1,10 @@
 import datetime
 from abc import ABC, abstractmethod
 from twisted.internet import reactor
+from typing import TYPE_CHECKING
 
+if TYPE_CHECKING:
+    from Strategies.OrderTypes import TradeDirection, OrderType  # Only for type hints
 
 class Exchange(ABC):
     api_url: str
@@ -13,20 +16,20 @@ class Exchange(ABC):
         self.asset = asset
         self.currency_asset = currency + asset
 
-    # abstract methods
+    # Abstract methods
 
     @abstractmethod
-    def get_connectivity_status():
+    def get_connectivity_status(self):
         pass
 
     @abstractmethod
-    def get_candle_stick_data() -> list[list]:
+    def get_candle_stick_data(self) -> list[list]:
         pass
 
     @abstractmethod
-    def get_account_status():
+    def get_account_status(self):
         pass
 
     @abstractmethod
-    def create_new_order():
+    def create_new_order(self, direction: "TradeDirection", order_type: "OrderType", quantity: int):
         pass
